186814e3ae6f9cc36c27263171d5c5a103e3ada7
FlorentinD <florentin.doerre@tu-dresden.de>
2020-08-10 19:45:22 +0200

    Fix reduce tests by using meaningful input .. before dimensions did not even match

--- main/ejml-dsparse/src/org/ejml/sparse/csc/MaskUtil_DSCC.java
+++ main/ejml-dsparse/src/org/ejml/sparse/csc/MaskUtil_DSCC.java
@@ -32,6 +32,7 @@ import static org.ejml.UtilEjml.checkSameShape;
 
 public class MaskUtil_DSCC {
     // for applying mask and accumulator (output gets overwritten)
+    // ! assumes that the mask is already applied to the output .. e.g. unset fields not even computed
     static DMatrixSparseCSC combineOutputs(DMatrixSparseCSC output, DBinaryOperator accum, DMatrixSparseCSC initialOutput) {
         if (initialOutput != null) {
             // memory overhead .. maybe also can reuse something?
